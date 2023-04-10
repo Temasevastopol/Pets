5595caebcd31ecabd7607c400f1e535e0cf086dc
Tema_sevastopol <temasebastopol@yandex.ru>
2023-04-10 22:09:33 +0300

    fix: use input 'change' + burger page_2 04/10/2023 22:09:31

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,14 @@
 const input = document.querySelector('input');
-const burger = document.querySelector('.burger')
 const html = document.querySelector('html');
 
 console.log(input, html)
 
 function isChecked(){ 
-    if(input.checked){
+    if(input.checked){ 
         html.style.overflow = 'hidden'
     }else{
         html.style.overflow = 'scroll';
     }
 }
 
-burger.addEventListener('click', ()=>{
-    setTimeout(isChecked, 100)
-});
+input.addEventListener('change', isChecked);
